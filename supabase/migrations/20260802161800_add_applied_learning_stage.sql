c1ed0e88ea861aa44515d35233978e76a8b310dc
Demon <muhammadans1255@gmail.com>
2026-08-02 21:26:47 +0500

    Add applied learning stage

---
+++ supabase/migrations/20260802161800_add_applied_learning_stage.sql
@@ -0,0 +1,12 @@
+alter table public.foundry_class_learning_notes
+  drop constraint if exists foundry_class_learning_notes_learning_state_check;
+
+alter table public.foundry_class_learning_notes
+  add constraint foundry_class_learning_notes_learning_state_check
+  check (learning_state = any (array[
+    'introduced'::text,
+    'practising'::text,
+    'understood'::text,
+    'applied'::text,
+    'mastered'::text
+  ]));
